00400c77e629774ce4dfc04641403f878a67c239
facundo thomas ramirez <facundorz2003@gmail.com>
2025-09-07 17:35:39 -0300

    nav centrado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Icono from '../img/Header/Icono.PNG'
 function Header(){
     return(
         <header class=" h-30 m-auto absolute w-full">
-            <div class="max-w-[1440px] flex justify-between items-center h-full px-5">
+            <div class="max-w-[1440px] flex justify-between items-center h-full px-5 m-auto">
                 <div class="w-28 h-20">
                     <img src={Icono} alt="" class="w-full h-full object-fit"/>
                 </div>
